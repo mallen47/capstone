e24a6d77f66339e571b7a1f4d59fd7b9c749b8a8
Matt Allen <allen.matt@gmail.com>
2025-09-23 20:53:21 -0400

    display symbols and balances on deposit form

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import Card from "react-bootstrap/Card"
 import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
@@ -7,13 +9,32 @@ import Dropdown from "react-bootstrap/Dropdown"
 import Button from "react-bootstrap/Button"
 
 const Deposit = () => {
+  const provider = useSelector(state => state.provider.connection)
+  const account = useSelector(state => state.provider.account)
+  const tokens = useSelector(state => state.tokens.contracts)
+  const symbols = useSelector(state => state.tokens.symbols)
+  const balances = useSelector(state => state.tokens.balances)
+
+  const amountHandler = async e => {
+    e.preventDefault()
+    console.log("amount handler...")
+  }
+
+  const depositHandler = async e => {
+    e.preventDefault()
+    console.log("deposit handler...")
+  }
+
   return (
     <div>
       <Card style={{ maxWidth: "450px" }} className="mx-auto px-4">
-        <Form style={{ maxWidth: "450px", margin: "50px auto" }}>
+        <Form
+          onSubmit={depositHandler}
+          style={{ maxWidth: "450px", margin: "50px auto" }}
+        >
           <Row>
             <Form.Text className="text-end my-2" muted>
-              Balance:
+              Balance: {balances[0]}
             </Form.Text>
             <InputGroup>
               <Form.Control
@@ -22,29 +43,36 @@ const Deposit = () => {
                 min="0.0"
                 step="any"
                 id="token1"
+                onChange={e => amountHandler(e)}
               />
               <InputGroup.Text
                 style={{ width: "100px" }}
                 className="justify-content-center"
-              ></InputGroup.Text>
+              >
+                {symbols && symbols[0]}
+              </InputGroup.Text>
             </InputGroup>
           </Row>
           <Row className="my-3">
             <Form.Text className="text-end my-2" muted>
-              Balance:
+              Balance: {balances[1]}
             </Form.Text>
-
             <InputGroup>
               <Form.Control
                 type="number"
                 placeholder="0.0"
                 step="any"
                 id="token2"
+                onChange={e => {
+                  amountHandler(e)
+                }}
               />
               <InputGroup.Text
                 style={{ width: "100px" }}
                 className="justify-content-center"
-              ></InputGroup.Text>
+              >
+                {symbols && symbols[1]}
+              </InputGroup.Text>
             </InputGroup>
           </Row>
           <Row className="my-4">
